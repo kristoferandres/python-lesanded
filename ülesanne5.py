b59ba7e75fb8cc9802f0f53334f224a1418955d7
kristoferandres <45108172+kristoferandres@users.noreply.github.com>
2022-02-15 09:23:35 +0200

    Add files via upload

--- ülesanne5.py
+++ ülesanne5.py
@@ -23,4 +23,36 @@ uus= input("sisesta uus nimi: ")
 nimed[valik-1] = uus
 print(nimed)
 
+print()
 
+#duplikaadid
+nimed = ['juhan','kati','mario','mario','mati','mati']
+prinditud = []
+for a in range(len(nimed)):
+    
+    if nimed[a] not in prinditud:
+        prinditud.append(nimed[a])
+for a in range(len(prinditud)):
+    print(f'{prinditud[a]}')
+    
+print()
+    
+#vanused
+kogusumma = 0
+vanused = ['12','15','64','13']
+vanused.sort()
+print(f'suurim arv: {vanused[-1]}')
+print(f'väikseim arv: {vanused[0]}')
+for a in range(len(vanused)):
+    kogusumma += int(vanused[a])
+print(f'kogusumma: {kogusumma}')
+print(f'keskmine: {kogusumma/len(vanused)}')
+
+print()
+
+#tärnid
+numbrid = ['6','2','3','4','5']
+for a in range(len(numbrid)):
+    print()
+    for b in range (0,int(numbrid[a])):
+        print('*', end = '')
